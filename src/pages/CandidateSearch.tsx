e2c1b9180ab161f2f7a3ae7d6d7efed3c1cbdb60
tlesner <tjlesner@gmail.com>
2025-03-19 20:00:28 -0600

    added more code

--- src/pages/CandidateSearch.tsx
+++ src/pages/CandidateSearch.tsx
@@ -4,61 +4,61 @@ import CandidateCard from '../components/CandidateCard';
 import type Candidate from '../interfaces/Candidate.interface';
 
 const CandidateSearch = () => {
-	// return <h1>CandidateSearch</h1>;
+  // return <h1>CandidateSearch</h1>;
 
-	//state
-	const [users, setUsers] = useState<Candidate[]>([]);
+  //state
+  const [users, setUsers] = useState<Candidate[]>([]);
   const [selectedUsers, setSelectedUsers] = useState<Candidate[]>([]);
-	const [currentUser, setCurrentUser] = useState<Candidate | undefined>(undefined);
+  const [currentUser, setCurrentUser] = useState<Candidate | undefined>(undefined);
 
-	//useEffect
-	useEffect(() => {
-		const initialize = async () => {
-			const data = await searchGithub();
-			return data;
-		};
-		initialize()
-			.then((result) => {
-				setUsers(result);
-				if (result.length > 0) {
-					setCurrentUser(result[0]);
-				}
-			})
-			.catch((err: any) => {
-				console.log('Error fetching users: ', err);
-			});
-	}, []);
-  
+  //useEffect
+  useEffect(() => {
+    const initialize = async () => {
+      const data = await searchGithub();
+      return data;
+    };
+    initialize()
+      .then((result) => {
+        setUsers(result);
+        if (result.length > 0) {
+          setCurrentUser(result[0]);
+        }
+      })
+      .catch((err: any) => {
+        console.log('Error fetching users: ', err);
+      });
+  }, []);
+
   useEffect(() => {
     console.log("Selected users: ", selectedUsers);
   }, [selectedUsers])
 
   const findUserIndex = () => {
     const currentIndex = users.findIndex(
-			u => u.id === currentUser?.id
-		);
+      u => u.id === currentUser?.id
+    );
 
-		if (currentIndex === undefined) {
-			console.log('Unable to find index with current user', currentUser);
-			return undefined;
-		}
-		if (currentIndex > users.length) {
-			console.log('No candidates are available to review, starting over.');
-			return 0;
-		}
+    if (currentIndex === undefined) {
+      console.log('Unable to find index with current user', currentUser);
+      return undefined;
+    }
+    if (currentIndex > users.length) {
+      console.log('No candidates are available to review, starting over.');
+      return 0;
+    }
 
     return currentIndex;
   }
 
-	//functions
-	const handleNextUser = () => {
-		const currentIndex = findUserIndex();
+  //functions
+  const handleNextUser = () => {
+    const currentIndex = findUserIndex();
     if (currentIndex === undefined) {
       return;
     }
 
-		setCurrentUser(users[currentIndex + 1]);
-	};
+    setCurrentUser(users[currentIndex + 1]);
+  };
 
   const handleSelectUser = () => {
     const currentIndex = findUserIndex();
@@ -66,29 +66,43 @@ const CandidateSearch = () => {
       return;
     }
 
-    const selectedUsersClone = [...selectedUsers]
-    
-    if (currentUser) {
-      selectedUsersClone.push(currentUser);
+    // const selectedUsersClone = [...selectedUsers]
+
+    // if (currentUser) {
+    //   selectedUsersClone.push(currentUser);
+    // }
+
+    // setSelectedUsers(selectedUsersClone);
+    // setCurrentUser(users[currentIndex + 1]);
+
+    let parsedCandidates = []
+    const storedCandidates = localStorage.getItem('storedCandidates');
+    if (typeof storedCandidates === 'string') {
+      parsedCandidates = JSON.parse(storedCandidates);
     }
+    parsedCandidates.push(currentUser);
+    localStorage.setItem(
+        'storedCandidates',
+        JSON.stringify(parsedCandidates)
+      );
+      
+      setCurrentUser(users[currentIndex + 1]);
 
-    setSelectedUsers(selectedUsersClone);
-    setCurrentUser(users[currentIndex + 1]);
-	};
+  };
 
-	return (
-		<div>
-			<h1>Candidate Search</h1>
-			{currentUser ? (
-				<div>
-					<CandidateCard username={currentUser.login} />
-					<button onClick={handleNextUser}>Reject User</button>
+  return (
+    <div>
+      <h1>Candidate Search</h1>
+      {currentUser ? (
+        <div>
+          <CandidateCard username={currentUser.login} />
+          <button onClick={handleNextUser}>Reject User</button>
 
           <button onClick={handleSelectUser}>Accept User</button>
-				</div>
-			) : null}
-		</div>
-	);
+        </div>
+      ) : null}
+    </div>
+  );
 };
 
 export default CandidateSearch;
--- src/pages/SavedCandidates.tsx
+++ src/pages/SavedCandidates.tsx
@@ -2,49 +2,39 @@ import { useState, useEffect } from "react";
 import { searchGithubUser } from "../api/API";
 
 
-type Props = {
-  usernames: string[];
-}
+const SavedCandidates = () => {
 
-const SavedCandidates = ({ usernames }: Props) => {
-  const [users, setUsers] = useState<any[]>([]);
+  const [users, setCandidateList] = useState([]);
 
-  useEffect(() => {
-    const init = async () => {
-      const loadedUsers: any[] = []
+  const removeFromStorage = (
+    e: { preventDefault: () => void; },
+    userId: any
+  ) => {
+    e.preventDefault();
+    if (userId !== undefined) {
+      let parsedCandidatesList = [];
 
-      for (let ii = 0; ii < usernames.length; ii++) {
-        const username = usernames[ii];
-        const user = await searchGithubUser(username);
+      const savedCandidatesList = localStorage.getItem('savedCandidates');
 
-        if (user) {
-          loadedUsers.push(user);
-        }
+      if (typeof savedCandidatesList === 'string') {
+        parsedCandidatesList = JSON.parse(savedCandidatesList)
       }
-      return loadedUsers;
-    };
 
-    init()
-      .then((result) => {
-        setUsers(result);
-      })
-      .catch((err) => {
-        console.log("🚀 Unable to find users:", err)
-      });
-  }, [usernames]);
+      parsedCandidatesList = parsedCandidatesList.filter(
+        (user) => user.id !== id
+      );
 
-  const handleRemoveUser = (userId: number) => {
-    const clonedUsers = [...users];
-
-    const userIndex = clonedUsers.findIndex(u => u.id === userId);
-    if (userIndex === undefined) {
-      return;
+      setCandidateList(parsedCandidatesList);
+      localStorage.setItem('savedCandidate', JSON.stringify(parsedCandidatesList));
     }
-    clonedUsers.splice(userIndex, 1)
+  }
+  useEffect(() => {
+    const parsedCandidatesList = JSON.parse(
+      localStorage.getItem('savedCandidates') as string
+    );
+    setCandidateList(parsedCandidatesList);
+  }, []);
 
-    setUsers(clonedUsers);
-
-  };
 
   return (
     <div>
@@ -72,13 +62,101 @@ const SavedCandidates = ({ usernames }: Props) => {
               <td>{user.location}</td>
               <td>{user.email}</td>
               <td>{user.bio}</td>
-              {/* <td>{user.}</td> */}
+              <td>
+                <button onClick={() =>
+                  removeFromStorage(user.id)}>
+                </button>
+              </td>
             </tr>
           )}
         </tbody>
       </table>
     </div>
   );
-};
+
+}
+
 
 export default SavedCandidates;
+
+
+// type Props = {
+//   usernames: string[];
+// }
+
+// const SavedCandidates = ({ usernames }: Props) => {
+//   const [users, setUsers] = useState<any[]>([]);
+
+
+//   // useEffect(() => {
+//   //   const init = async () => {
+//   //     const loadedUsers: any[] = []
+
+//   //     for (let ii = 0; ii < usernames.length; ii++) {
+//   //       const username = usernames[ii];
+//   //       const user = await searchGithubUser(username);
+
+//   //       if (user) {
+//   //         loadedUsers.push(user);
+//   //       }
+//   //     }
+//   //     return loadedUsers;
+//   //   };
+
+//   //   init()
+//   //     .then((result) => {
+//   //       setUsers(result);
+//   //     })
+//   //     .catch((err) => {
+//   //       console.log("🚀 Unable to find users:", err)
+//   //     });
+//   // }, [usernames]);
+
+//   // const handleRemoveUser = (userId: number) => {
+//   //   const clonedUsers = [...users];
+
+//   //   const userIndex = clonedUsers.findIndex(u => u.id === userId);
+//   //   if (userIndex === undefined) {
+//   //     return;
+//   //   }
+//   //   clonedUsers.splice(userIndex, 1)
+
+//   //   setUsers(clonedUsers);
+
+//   // };
+
+//   return (
+//     <div>
+//       <h1>Potential Candidates</h1>
+//       <table className="table">
+//         <tbody className="tbody">
+//           <tr className="tr">
+//             <th>Image</th>
+//             <th>Name</th>
+//             <th>Location</th>
+//             <th>Email</th>
+//             <th>Company</th>
+//             <th>Bio</th>
+//             <th>Remove</th>
+//           </tr>
+//           {users.map((user, index) =>
+//             <tr>
+//               <td>
+//                 <img
+//                   src={user.avatar_url}
+//                 >
+//                 </img>
+//               </td>
+//               <td>{user.name}</td>
+//               <td>{user.location}</td>
+//               <td>{user.email}</td>
+//               <td>{user.bio}</td>
+//               {/* <td>{user.}</td> */}
+//             </tr>
+//           )}
+//         </tbody>
+//       </table>
+//     </div>
+//   );
+// };
+
